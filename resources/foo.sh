0ab53b39c21cc396b61a67e1c5d847a3863b07d7
esteveslima-solvimm <esteves.lima@solvimm.com>
2021-09-03 15:06:43 -0300

    fix - update custom env var test

--- resources/foo.sh
+++ resources/foo.sh
@@ -4,3 +4,6 @@ echo 'some script from resource'
 echo "$SOME_ENV_VAR"
 echo '---'
 echo "$JOB_NAME"
+echo '---'
+
+echo "$JOB_URL_NAME"
--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,8 +1,12 @@
 // call with foo(anyParameter: "value", ...)
-def call(Map config = [:]) {
+def call(Map config = [:]) {    
     sh "echo 'vars script!'"
     sh "echo $SOME_ENV_VAR"
     sh "echo ---"
     def configStr = config.inspect()
     sh "echo parameters: ${configStr}"
+
+    // creat custom env var
+    env.JOB_URL_NAME = env.JOB_URL.replaceFirst(/^.*\/([^\/]+?)$/, '$1')
+    sh "echo $JOB_URL_NAME"
 }
